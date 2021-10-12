71c93aa0abc42248d5b1deec0fcea206002a7422
Lovato17 <79473205+Lovato17@users.noreply.github.com>
2021-10-12 16:25:48 -0300

    Adicionado API CEP e Cadastro das informaçoes do Usuario

--- wr-lazer/MetodosDAO.php
+++ wr-lazer/MetodosDAO.php
@@ -15,9 +15,17 @@ class wrlazer
                echo "<br><center>EMAIL OU SENHA INCORRETOS</center>";
           }
      }
-     function cadastrar(){
-          global $conexao;
+     function cadastrar($nome,$sobrenome,$email,$senha,$dt_nas,$cpf,$cep,$bairro,$rua,$cidade,$numero_end,$complemento,$sexx  ){
+          global $conexao; 
 
+          try {    
+               $nome = mysqli_real_escape_string($conexao,$nome);              
+               mysqli_query($conexao,"INSERT INTO `tb_cliente` (id_cliente, `nome_cliente`, `sobrenome_cliente`, `email_cliente`, `senha_cliente`, `dt_nascimento_cliente`, `cpf_cliente`, `cep_cliente`, `bairro_cliente`, `rua_cliente`, `cidade_cliente`, `numero_end_cliente`, `complemento_end_cliente`, `fk_sexo`) VALUES (NULL, '$nome', '$sobrenome', '$email', '$senha', '$dt_nas', '$cpf', '$cep', '$bairro', '$rua', '$cidade', '$numero_end', '$complemento', '$sexx');");
+     
+              } catch (Exception $erro) {
+              echo "Erro - " . $erro;
+          }
+          
      }
 }
 
--- wr-lazer/cadastro.php
+++ wr-lazer/cadastro.php
@@ -1,12 +1,4 @@
-<?php
-function get_endereco($cep){
-    $cep = preg_replace("/[^0-9]/", "", $cep);
-    $url = "http://viacep.com.br/ws/$cep/xml/";
-    $xml = simplexml_load_file($url);
-    return $xml;
-}
-
-?>
+<?php include 'conexao.php';?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,25 +6,64 @@ function get_endereco($cep){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <!-- Adicionando JQuery -->
+        <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+        <script type="text/javascript" src="js/api_cep.js"></script>
+
     </head>
     <body>
-        <form action="" method="post">
-            <input type="name" name="txt_nome" placeholder="NOME"><br>
-            <input type="text" name="txt_sobrenome" placeholder="SOBRENOME"><br>
-            <input type="text" name="txt_email" placeholder="E-MAIL"><br>
-            <input type="text" name="txt_senha" placeholder="SENHA"><br>
-            <input type="text" name="txt_dt_nas" placeholder="DATA DE NASCIMENTO"><br>
-            <input type="text" name="txt_tel" placeholder="TELEFONE"><br>
-            <input type="text" name="txt_sexo" placeholder="SEXO"><br>
-            <input type="text" name="txt_cep" placeholder="CPF"><br>
-            <input type="text" name="txt_cep" placeholder="CEP">
-            <input type="text" name="txt_bairro" placeholder="BAIRRO"><br>
-            <input type="text" name="txt_rua" placeholder="RUA"><br>
-            <input type="text" name="txt_cidade" placeholder="CIDADE"><br>
-            <input type="text" name="txt_numero_end" placeholder="NUMERO DO ENDEREÇO"><br>
-            <input type="text" name="txt_complemento" placeholder="COMPLEMENTO"><br>
-            <input type="submit" name="btn_cadastra" value="CADASTRAR">
+        <form action="" method="POST">
+            <input type="name" name="nome" placeholder="NOME"><br>
+            <input type="text" name="sobrenome" placeholder="SOBRENOME"><br>
+            <input type="email" name="email" placeholder="E-MAIL"><br>
+            <input type="text" name="senha" placeholder="SENHA"><br>
+            <input type="text" name="dt_nas" placeholder="DATA DE NASCIMENTO"><br>
+            <select name="select_sex" id="">
+                <option >SELECIONE</option>
+                <?php $SELECT = "SELECT*FROM tb_sexo"; $RESULTADO = mysqli_query($conexao,$SELECT);
+                while($sexo = mysqli_fetch_assoc($RESULTADO)){ ?>
+                <option  value="<?php echo $sexo['id_sexo']; ?>"><?php echo $sexo['sexo']; ?></option>
+                <?php } ?>
+            </select><br><br><br><br><br><br>
+            <input type="text" name="cpf" placeholder="CPF"><br>
+            <input type="text" name="cep" placeholder="CEP" id="cep" value=""><br>
+            <input type="text" name="rua" placeholder="RUA" id="rua" value=""><br>
+            <input type="text" name="bairro" placeholder="BAIRRO" id="bairro" value=""><br>
+            <input type="text" name="cidade" placeholder="CIDADE" id="cidade" value=""><br>
+            <input type="text" name="uf" placeholder="UF" id="uf" size="2" value=""><br>
+            <input type="text" name="numero_end" placeholder="NUMERO DO ENDEREÇO"><br>
+            <input type="text" name="complemento" placeholder="COMPLEMENTO"><br>
+            <input type="submit" name="btn_cadastrar" value="CADASTRAR">
         </form>
+
+        <?php 
+         require_once 'MetodosDAO.php';
+         $wrlazer = new Wrlazer;
+
+         
+        if(isset($_POST['btn_cadastrar'])){
+
+            $nome =$_POST['nome'];
+            $sobrenome =$_POST['sobrenome'];
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+            $dt_nas = $_POST['dt_nas'];
+            $cpf = $_POST['cpf'];
+            $cep = $_POST['cep'];
+            $bairro = $_POST['bairro'];
+            $rua = $_POST['rua'];
+            $cidade = $_POST['cidade'];
+            $uf = $_POST['uf'];
+            $numero_end = $_POST['numero_end'];
+            $complemento = $_POST['complemento'];
+            $sexx = $_POST['select_sex'];
+
+            $wrlazer->cadastrar($nome,$sobrenome,$email, $senha,$dt_nas,$cpf,$cep ,$bairro,$rua ,$cidade,$numero_end ,$complemento,$sexx );
+        }
+        ?>
 
     </body>
 </html>
