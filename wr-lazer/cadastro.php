c0c0671c57bf7c792e9a0d417da55609868dd8b2
AllanAbranches <74166301+AllanAbranches@users.noreply.github.com>
2021-11-19 22:34:40 -0300

    DESIGN TELA LOG E CAD
    Css da tela Login e cadastro inciado

--- wr-lazer/cadastro.php
+++ wr-lazer/cadastro.php
@@ -5,7 +5,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel = "stylesheet" href = "css/cad.css" > 
+        <title>Cadastro</title>
         <!-- Adicionando JQuery -->
         <script
             src="https://code.jquery.com/jquery-3.6.0.min.js"
@@ -15,37 +16,48 @@
 
     </head>
     <body>
-        <form action="" method="POST">
-            <input type="name" name="nome" placeholder="NOME"><br>
-            <input type="text" name="sobrenome" placeholder="SOBRENOME"><br>
-            <input type="email" name="email" placeholder="E-MAIL"><br>
-            <input type="text" name="senha" placeholder="SENHA"><br>
-            <input type="text" name="dt_nas" placeholder="DATA DE NASCIMENTO"><br>
-            <select name="select_sex" id="">
-                <option >SELECIONE</option>
-                <?php $SELECT = "SELECT*FROM tb_sexo"; $RESULTADO = mysqli_query($conexao,$SELECT);
-                while($sexo = mysqli_fetch_assoc($RESULTADO)){ ?>
-                <option  value="<?php echo $sexo['id_sexo']; ?>"><?php echo $sexo['sexo']; ?></option>
-                <?php } ?>
-            </select><br><br><br><br><br><br>
-            <input type="text" name="cpf" placeholder="CPF"><br>
-            <input type="text" name="cep" placeholder="CEP" id="cep" value=""><br>
-            <input type="text" name="rua" placeholder="RUA" id="rua" value=""><br>
-            <input type="text" name="bairro" placeholder="BAIRRO" id="bairro" value=""><br>
-            <input type="text" name="cidade" placeholder="CIDADE" id="cidade" value=""><br>
-            <input type="text" name="uf" placeholder="UF" id="uf" size="2" value=""><br>
-            <input type="text" name="numero_end" placeholder="NUMERO DO ENDEREÇO"><br>
-            <input type="text" name="complemento" placeholder="COMPLEMENTO"><br>
-            <input type="submit" name="btn_cadastrar" value="CADASTRAR">
-        </form>
-
+<center>
+      <div class="form-cad-fundo">
+        <img src="../wr-lazer/img/logo.png" alt="">
+         <div class="form-cad-pt">
+            <div class="form-cad-pt2">
+                <form action="" method="POST">
+                    <input class="caixas-cad" type="name" name="nome" placeholder="NOME"><br>
+                    <input class="caixas-cad" type="text" name="sobrenome" placeholder="SOBRENOME"><br>
+                    <input class="caixas-cad" type="email" name="email" placeholder="E-MAIL"><br>
+                    <input class="caixas-cad" type="text" name="senha" placeholder="SENHA"><br>
+                    <input class="caixas-cad" type="text" name="dt_nas" placeholder="DATA DE NASCIMENTO"><br>
+                    <select class="caixas-cad" name="select_sex" id="">
+                        <option >SELECIONE</option>
+                        <?php $SELECT = "SELECT*FROM tb_sexo"; $RESULTADO = mysqli_query($conexao,$SELECT);
+                        while($sexo = mysqli_fetch_assoc($RESULTADO)){ ?>
+                        <option  value="<?php echo $sexo['id_sexo']; ?>"><?php echo $sexo['sexo']; ?></option>
+                        <?php } ?>
+                    </select><br><br><br><br><br><br>
+             </div>
+             <div class="form-cad-pt2">
+                <input class="caixas-cad" type="text" name="cpf" placeholder="CPF"><br>
+                    <input class="caixas-cad" type="text" name="cep" placeholder="CEP" id="cep" value=""><br>
+                    <input class="caixas-cad" type="text" name="rua" placeholder="RUA" id="rua" value=""><br>
+                    <input class="caixas-cad" type="text" name="bairro" placeholder="BAIRRO" id="bairro" value=""><br>
+                    <input class="caixas-cad" type="text" name="cidade" placeholder="CIDADE" id="cidade" value=""><br>
+                    <input class="caixas-cad" type="text" name="uf" placeholder="UF" id="uf" size="2" value=""><br>
+                    <input class="caixas-cad" type="text" name="numero_end" placeholder="NUMERO DO ENDEREÇO"><br>
+                    <input class="caixas-cad" type="text" name="complemento" placeholder="COMPLEMENTO"><br>
+                </form>
+             </div>          
+         </div>
+                  <div class="mov-btn-cad">
+                    <input class="btn-cad" type="submit" name="btn_cadastrar" value="CADASTRAR">
+                  </div>     
+      </div>
+    </center>
         <?php 
          require_once 'MetodosDAO.php';
          $wrlazer = new Wrlazer;
 
          
         if(isset($_POST['btn_cadastrar'])){
-
             $nome =$_POST['nome'];
             $sobrenome =$_POST['sobrenome'];
             $email = $_POST['email'];
--- wr-lazer/login.php
+++ wr-lazer/login.php
@@ -4,21 +4,28 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel = "stylesheet" href = "css/log.css" >
+        <title>Login</title>
 
         
     </head>
     <body>
         <center>
-
-        <form action="" method="post">
-            <input type="email" placeholder="E-mail" name="txt_email"><br>
-            <input type="password" placeholder="Senha" name="txt_senha"><br>
-            <input type="submit" name="btn_entrar"><br><br><br>
-            <a href="cadastro.php">AINDA NAO SE CADASTROU? CADASTRE-SE</a>
-        </form>
-            
+    <div class="form-log-fundo">
+        <img src="../wr-lazer/img/logo.png" alt="">
+        <div class="form-log-pt">
+            <form action="" method="post">
+                <input class="caixas-log" type="email" placeholder="E-mail" name="txt_email"><br>
+                <input class="caixas-log" type="password" placeholder="Senha" name="txt_senha"><br>
+            </form>
+            <input class="caixas-log" type="submit" name="btn_entrar"><br><br><br>
+                <a class="link-log" href="cadastro.php">AINDA NAO SE CADASTROU? CADASTRE-SE</a>
+                
+        </div>
+    </div>
+       
         </center>
+        
         <?php            
             require_once 'MetodosDAO.php';
             $wrlazer = new Wrlazer;
