dd7b0eebc48eff3e4ffa120c4eb67b925f8f7b68
Lovato17 <79473205+Lovato17@users.noreply.github.com>
2021-12-08 02:28:57 -0300

    Curso

--- wr-lazer/cadastroCliente.php
+++ wr-lazer/cadastroCliente.php
@@ -13,7 +13,79 @@
             crossorigin="anonymous"></script>
         <script src="./js/api_cep.js"></script>
         <script src="./js/maskdocument.js"></script>
-        
+            <!-- Adicionando JQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+
+    <!-- Adicionando Javascript -->
+    <script>
+
+        $(document).ready(function() {
+
+            function limpa_formulário_cep() {
+                // Limpa valores do formulário de cep.
+                $("#rua").val("");
+                $("#bairro").val("");
+                $("#cidade").val("");
+                $("#uf").val("");
+                $("#ibge").val("");
+            }
+            
+            //Quando o campo cep perde o foco.
+            $("#cep").blur(function() {
+
+                //Nova variável "cep" somente com dígitos.
+                var cep = $(this).val().replace(/\D/g, '');
+
+                //Verifica se campo cep possui valor informado.
+                if (cep != "") {
+
+                    //Expressão regular para validar o CEP.
+                    var validacep = /^[0-9]{8}$/;
+
+                    //Valida o formato do CEP.
+                    if(validacep.test(cep)) {
+
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#rua").val("...");
+                        $("#bairro").val("...");
+                        $("#cidade").val("...");
+                        $("#uf").val("...");
+                        $("#ibge").val("...");
+
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#rua").val(dados.logradouro);
+                                $("#bairro").val(dados.bairro);
+                                $("#cidade").val(dados.localidade);
+                                $("#uf").val(dados.uf);
+                                $("#ibge").val(dados.ibge);
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        //cep é inválido.
+                        limpa_formulário_cep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulário_cep();
+                }
+            });
+        });
+
+    </script>
         <title>Cadastro</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
@@ -34,7 +106,7 @@
                 <div class="fs-2 mb-3">
                         <h1>CADASTRO</h1><br>
                 </div>
-                <form class="row g-3" >
+                <form class="row g-3" action="" method="POST" >
                     <div class="col-md-6">
                         <label for="inputName" class="form-label">Nome:</label>
                         <input  name="nome" type="nome" class="form-control" id="inputName" placeholder="Insira o seu nome">
@@ -59,7 +131,7 @@
                     </div>
                     <div class="col-md-6">
                         <label for="inputDate" class="form-label">Data de nascimento:</label>
-                        <input id="dtnasc" name="dtnasc" placeholder="DD/MM/AAAA" class="form-control input-md" required="" type="text" maxlength="10" OnKeyPress="formatar('##/##/####', this)" onBlur="showhide()">
+                        <input id="dtnasc" name="dt_nasc" placeholder="DD/MM/AAAA" class="form-control input-md" required="" type="text" maxlength="10" OnKeyPress="formatar('##/##/####', this)" onBlur="showhide()">
                     </div>
                     <br><br><br><br>
                     <div class="col-md-6">
@@ -69,7 +141,7 @@
                     <br><br><br><br>
                     <div class="col-md-6">
                         <label for="inputNumberHouse" class="form-label">Nº:</label>
-                        <input name="numero_end_cliente" type="text" class="form-control" id="numero_end_cliente" placeholder="">
+                        <input name="numero_end" type="text" class="form-control" id="numero_end_cliente" placeholder="">
                     </div>
                     <div class="col-md-6">
                         <label for="inputDistrict" class="form-label">Bairro:</label>
@@ -87,8 +159,9 @@
                 
                     <div class="col-md-2" >
                     <label for="inputState" class="form-label">UF:</label>
-                        <select name="uf" id="uf" class="form-select" style="width: 60px; height: 30px; ">
+                        <select name="uf" class="form-select" id="uf"  style="width: 60px; height: 30px; ">
                             <option selected></option>
+                        
                         </select><br><br><br>
                     </div>
                     <div class="col-12">
@@ -108,7 +181,7 @@
       <section class="">
         <p class="d-flex justify-content-center align-items-center">
           <span class="me-3"><p></p> Entre com a sua conta: <p></p></span>
-          <button type="button" class="btn btn-outline-light btn-rounded">
+          <input type="submit" name="btn_cadastrar" class="btn btn-outline-light btn-rounded" >
             <a style="color: white;" href="login.php">Login</a>
           </button>
         </p>
